287d937090558697f66cd0d73443891b12a51a6c
KennyLi6 <kennyli3219@gmail.com>
2025-01-26 19:06:46 -0800

    created behavior tree with empty function calls and a couple of checks used

--- behavior_tree_bot/bt_bot.py
+++ behavior_tree_bot/bt_bot.py
@@ -26,17 +26,35 @@ def setup_behavior_tree():
     # Top-down construction of behavior tree
     root = Selector(name='High Level Ordering of Strategies')
 
-    offensive_plan = Sequence(name='Offensive Strategy')
+    close_plan = Selector(name='Close Planet Strategy')
+    
+    close_spread_sequence = Sequence(name='Close Spread Strategy')
+    close_neutral_planet_check = Check(if_close_neutral_planet_available)
+    close_spread_action = Action(spread_to_close_weakest_neutral_planet)
+
+    close_attack_sequence = Sequence(name='Close Attack Strategy')
+    close_enemy_planet_check = Check(if_close_weak_enemy_planet)
+    close_attack_action = Action(attack_close_weakest_enemy_planet)
+
+    close_plan.child_nodes = [close_spread_sequence, close_attack_sequence]
+    close_spread_sequence.child_nodes = [close_neutral_planet_check, close_spread_action]
+    close_attack_sequence.child_nodes = [close_enemy_planet_check, close_attack_action]
+
+    far_plan = Sequence(name='Far Planet Strategy')
+
+    far_spread_sequence = Sequence(name='Far Spread Strategy')
+    neutral_planet_check = Check(if_neutral_planet_available)
+    far_spread_action = Action(spread_to_weakest_neutral_planet)
+
+    far_attack_sequence = Sequence(name='Far Attack Strategy')
     largest_fleet_check = Check(have_largest_fleet)
     attack = Action(attack_weakest_enemy_planet)
-    offensive_plan.child_nodes = [largest_fleet_check, attack]
 
-    spread_sequence = Sequence(name='Spread Strategy')
-    neutral_planet_check = Check(if_neutral_planet_available)
-    spread_action = Action(spread_to_weakest_neutral_planet)
-    spread_sequence.child_nodes = [neutral_planet_check, spread_action]
-
-    root.child_nodes = [offensive_plan, spread_sequence, attack.copy()]
+    far_plan.child_nodes = [far_spread_sequence, far_attack_sequence]
+    far_spread_sequence.child_nodes = [neutral_planet_check, far_spread_action]
+    far_attack_sequence.child_nodes = [largest_fleet_check, attack]
+    
+    root.child_nodes = [close_plan, far_plan, attack.copy()]
 
     logging.info('\n' + root.tree_to_string())
     return root
--- behavior_tree_bot/checks.py
+++ behavior_tree_bot/checks.py
@@ -1,11 +1,49 @@
+import logging
 
+MAX_DISTANCE = 10
 
 def if_neutral_planet_available(state):
     return any(state.neutral_planets())
 
+def if_close_neutral_planet_available(state):
+    # Don't bother checking if no neutral planets
+    if not if_neutral_planet_available(state):
+        return False
+
+    # Find my strongest planet.
+    strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
+    if not strongest_planet:
+        return False
+    # Find all planets that are within MAX_DISTANCE
+    close_planets = [p for p in state.neutral_planets() if state.distance(strongest_planet.ID, p.ID) < MAX_DISTANCE]
+
+    if len(close_planets) < 1:
+        # No legal source or destination
+        return False
+    else:
+        # Can spread to a close neutral planet
+        return True
+
+def if_close_weak_enemy_planet(state):
+    if not have_largest_fleet(state):
+        return False
+    # Find my strongest planet.
+    strongest_planet = max(state.my_planets(), key=lambda p: p.num_ships, default=None)
+    if not strongest_planet:
+        return False
+    # Find all planets that are within MAX_DISTANCE
+    close_planets = [p for p in state.enemy_planets() if state.distance(strongest_planet.ID, p.ID) < MAX_DISTANCE]
+
+    if len(close_planets) < 1:
+        # No legal source or destination
+        return False
+    else:
+        # Can attack close enemy planet
+        return True
 
 def have_largest_fleet(state):
     return sum(planet.num_ships for planet in state.my_planets()) \
              + sum(fleet.num_ships for fleet in state.my_fleets()) \
            > sum(planet.num_ships for planet in state.enemy_planets()) \
              + sum(fleet.num_ships for fleet in state.enemy_fleets())
+
